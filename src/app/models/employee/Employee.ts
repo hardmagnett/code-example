def4801014425530bad1195225b50b6db14117ad
Vadim Yemtsov <hardmagnett@yandex.ru>
2025-07-06 16:36:49 +0300

    сделал ORM-модели без декораторов

--- src/app/models/employee/Employee.ts
+++ src/app/models/employee/Employee.ts
@@ -3,15 +3,30 @@ import { BelongsTo, Str, Uid, Num } from "pinia-orm/decorators";
 import Position from "@/app/models/position/Position";
 export default class Employee extends Model {
   static entity = "employees";
-
-  @Uid() declare id: number;
-
-  @Str("") declare firstname: string;
-  @Str("") declare lastname: string;
-
-  @Num(null) declare position_id: number;
-
-  @BelongsTo(() => Position, "position_id") declare position: Position | null;
+  
+  static fields(){
+    return {
+      id: this.uid(),
+      firstname: this.string(''),
+      lastname: this.string(''),
+      position_id: this.attr(null),
+      position: this.belongsTo(Position, 'position_id')
+    }
+  }
+  declare id: string
+  declare firstname: string
+  declare lastname: string
+  declare position_id: string | null
+  declare position: Position | null
+  
+  // @Uid() declare id: number;
+  //
+  // @Str("") declare firstname: string;
+  // @Str("") declare lastname: string;
+  //
+  // @Num(null) declare position_id: number;
+  //
+  // @BelongsTo(() => Position, "position_id") declare position: Position | null;
 
   get fullname() {
     return `${this.firstname} ${this.lastname}`;
--- src/app/models/position/Position.ts
+++ src/app/models/position/Position.ts
@@ -2,6 +2,22 @@ import { Model } from "pinia-orm";
 import { Str, Num } from "pinia-orm/decorators";
 export default class Position extends Model {
   static entity = "positions";
-  @Num(null) declare id: number;
-  @Str("") declare title: string;
+
+  static fields(){
+    return {
+      id: this.uid(),
+      title: this.string(''),
+      // lastname: this.string(''),
+      // position_id: this.attr(null),
+      // position: this.belongsTo(Position, 'position_id')
+    }
+  }
+  declare id: number
+  declare title: string
+  // declare lastname: string
+  // declare position_id: string | null
+  // declare position: Position | null
+  
+  // @Num(null) declare id: number;
+  // @Str("") declare title: string;
 }
